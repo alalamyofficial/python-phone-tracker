afa91f30b9be4a2c23b92f9ffec95debfad77b45
alalamyofficial <62678972+alalamyofficial@users.noreply.github.com>
2021-01-07 09:40:47 +0200

    Update python-tracker-gui.py

--- python-tracker-gui.py
+++ python-tracker-gui.py
@@ -22,6 +22,7 @@ class Location_Tracker:
         self.country_label = Label(App,fg="white", font=("Times", 20), bg="#3f5efb")
         self.timezone_label = Label(App,fg="white", font=("Times", 20), bg="#3f5efb")
         self.svp_label = Label(App,fg="white", font=("Times", 20), bg="#3f5efb")
+        self.clearbutton = Button(App,text="Clear", bg="#23d1c3",font = ('Sans','10','bold'),command=self.clearbtn)
 
         #___________Place widgets on the window______
         self.phone_number.place(x=150, y=120)
@@ -30,6 +31,7 @@ class Location_Tracker:
         self.country_label.place(x=200, y=230)
         self.timezone_label.place(x=200, y=275)
         self.svp_label.place(x=200, y=325)
+        self.clearbutton.place(x=50,y=250)
 
         #__________Linking button with countries ________
         self.track_button.bind("<Button-1>", self.Track_location)
@@ -40,37 +42,40 @@ class Location_Tracker:
             phone_number = self.phone_number.get()
             # country = "Country is Unknown"
             if phone_number is not None:
-                if(type(phone_number) is int):
-                    #__________for country name______________
-                    mainNum = phonenumbers.parse(phone_number,"CH") 
-                    if mainNum:
-                        tracked = geocoder.description_for_number(mainNum,"en") 
-                    else:
-                        print("error")
-                    #__________For Timezone ______________
-                    phoneNumber = phonenumbers.parse(phone_number) 
-                    if phoneNumber: 
-                        timeZone = timezone.time_zones_for_number(phoneNumber) 
-                    else:
-                        print("error") 
-                    #__________for Service Provider name______________
+                
+                #__________for country name______________
+                mainNum = phonenumbers.parse(phone_number,"CH") 
+                if mainNum:
+                    tracked = geocoder.description_for_number(mainNum,"en") 
+                else:
+                    print("error")
+                #__________For Timezone ______________
+                phoneNumber = phonenumbers.parse(phone_number) 
+                if phoneNumber: 
+                    timeZone = timezone.time_zones_for_number(phoneNumber) 
+                else:
+                    print("error") 
+                #__________for Service Provider name______________
 
-                    service_number = phonenumbers.parse(phone_number,"RO")
-                    if service_number:
-                        svp = carrier.name_for_number(service_number,"en")
-                    else:
-                        print("error")
+                service_number = phonenumbers.parse(phone_number,"RO")
+                if service_number:
+                    svp = carrier.name_for_number(service_number,"en")
+                else:
+                    print("error")
 
-                    self.country_label.configure(text=tracked)
-                    self.timezone_label.configure(text=timeZone)
-                    self.svp_label.configure(text=svp)
+                self.country_label.configure(text=tracked)
+                self.timezone_label.configure(text=timeZone)
+                self.svp_label.configure(text=svp)
 
-            # elif len(self.phone_number.get()) == 0:
             else:
                 phone_number.focus_set()
 
         except:
             print("you must put a valid number")
+
+    def clearbtn(self):
+        self.phone_number.delete(0, 'end')
+        # print("clear")
 
 PhoneTracker = Tk()
 MyApp = Location_Tracker(PhoneTracker)
